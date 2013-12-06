08f90150214a2c4659cbd33ae868677ca17a3ce6
John Hainline <jihainline@gmail.com>
2013-12-06 16:34:53 -0600

    Experimenting with syntax.

--- Stubble/STBMock.h
+++ Stubble/STBMock.h
@@ -5,21 +5,35 @@
 //
 //STBMockObject *when(id mock);
 
-
-
-
-
-
+// ********************
+// Mockito syntax
 //when(mockedList.get(0)).thenReturn("first");
+
+
+
+
+// ********************
+// Mock objects have verify, reject, stub methods
+
+//[[mockObject verify:times(2)] myMethod:@"Blargh"];
+
+//[[mockObject.when myMethod:@"Blargh" param2:any()] thenReturn:@"Ack"];
+//
+//[[[mockObject when] myMethod:@"Blargh" param2:any()] thenReturn:@"Ack"];
 //
 //
+//when([mockObject myMethod:@"Blargh"]);
 //
-//
-//
-//
-//[reject(mockObject) myMethod:@"blargh"];
+
+//[[when.mockObject myMethod:@"Blargh" param2:any()] thenReturn:@"DGnadosg"]
+
+// ********************
+// All calls are C and look like verify(mockObject), or any(), etc.
 //
 //[verify(mockObject, times(2)) myMethod:any()];
+//[verify(mockObject, never()) myMethod:any()];
+//
+//[[when(mockObject) myMethod:@"Blargh" param2:any() param3:captor(&myInt)] thenReturn:@"Baz"];
 //
 //[[stub(mockObject) doStuff:@"Blah"] andReturn:@"Baz"];
 //
