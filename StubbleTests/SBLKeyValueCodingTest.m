cddcfb1c2c410bfcf7e226275f5a205f7ea8e73e
Micah Hainline <micah.hainline@gmail.com>
2015-08-12 14:46:15 -0500

    Uncommented test around KVC being used in predicates.

--- StubbleTests/SBLKeyValueCodingTest.m
+++ StubbleTests/SBLKeyValueCodingTest.m
@@ -29,20 +29,20 @@
 	XCTAssertEqualObjects(value1, @1337L);
 }
 
-//- (void)testWhenValuesAreStubbedThenTheyCanBeUsedInAPredicate {
-//	SBLTestingClass *mock1 = mock(SBLTestingClass.class);
-//	[when(mock1.methodReturningLong) thenReturn:@1L];
-//	SBLTestingClass *mock2 = mock(SBLTestingClass.class);
-//	[when(mock2.methodReturningLong) thenReturn:@2L];
-//	SBLTestingClass *mock3 = mock(SBLTestingClass.class);
-//	[when(mock3.methodReturningLong) thenReturn:@3L];
-//
-//	NSArray *array = @[mock1, mock2, mock3];
-//
-//	NSPredicate *predicate = [NSPredicate predicateWithFormat:@"methodReturningLong >= 2"];
-//	NSArray *filteredArray = [array filteredArrayUsingPredicate:predicate];
-//
-//	XCTAssertEqualObjects(filteredArray, (@[mock2, mock3]));
-//}
+- (void)testWhenValuesAreStubbedThenTheyCanBeUsedInAPredicate {
+	SBLTestingClass *mock1 = mock(SBLTestingClass.class);
+	[when(mock1.methodReturningLong) thenReturn:@1L];
+	SBLTestingClass *mock2 = mock(SBLTestingClass.class);
+	[when(mock2.methodReturningLong) thenReturn:@2L];
+	SBLTestingClass *mock3 = mock(SBLTestingClass.class);
+	[when(mock3.methodReturningLong) thenReturn:@3L];
+
+	NSArray *array = @[mock1, mock2, mock3];
+
+	NSPredicate *predicate = [NSPredicate predicateWithFormat:@"methodReturningLong >= 2"];
+	NSArray *filteredArray = [array filteredArrayUsingPredicate:predicate];
+
+	XCTAssertEqualObjects(filteredArray, (@[mock2, mock3]));
+}
 
 @end
